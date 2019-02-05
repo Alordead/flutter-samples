a2bc0d5f8b2ccb2b2af9e9cf9b874799f8364ecf
Alordead <spealcy@gmail.com>
2019-02-05 21:48:18 +0300

    todo: bug fixes, catalog update

---
+++ flutter_todo_app/lib/todo.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'todo_list_screen.dart';
+
+final _rowHeight = 60.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
+class Todo extends StatefulWidget {
+  String title;
+  bool editing;
+  int index;
+
+  Todo({this.title, this.editing, this.index});
+
+  @override
+  State<StatefulWidget> createState() {
+    return TodoState(todo: this);
+  }
+}
+
+class TodoState extends State<Todo> with TickerProviderStateMixin {
+  Todo todo;
+
+  AnimationController controller;
+  Animation<double> animation;
+
+  bool editing = true;
+  bool readyButtonPressed = false;
+
+  TextEditingController _textEditingController = TextEditingController();
+
+  TodoState({this.todo});
+
+  initState() {
+    super.initState();
+
+    controller = AnimationController(
+        duration: const Duration(milliseconds: 500),
+        vsync: this
+    );
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      direction: DismissDirection.endToStart,
+      key: Key(todo.title),
+      onDismissed: (direction) {
+        todos.removeAt(todo.index);
+        count -= 1;
+      },
+      child: Material(
+        color: Colors.transparent,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 3.0),
+          child: FadeTransition(
+            opacity: animation,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(80.0),
+              ),
+              color: Colors.white,
+              elevation: 6.0,
+              child: InkWell(
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                borderRadius: _borderRadius,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10.0),
+                          child: TextField(
+                            style: TextStyle(
+                              color: readyButtonPressed ? Colors.grey[500] : Colors.black,
+                              decoration: readyButtonPressed ? TextDecoration.lineThrough : null,
+                              fontSize: 20.0,
+                            ),
+                            enabled: editing ? true : false,
+                            controller: _textEditingController,
+                            maxLengthEnforced: false,
+                            onSubmitted: _handleSubmitted,
+                            decoration: InputDecoration.collapsed(
+                                hintText: "Write your task here..."
+                            ),
+                          ),
+                        ),
+                      ),
+                      Card(
+                        color: readyButtonPressed ? Colors.green[400] : Colors.grey,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        child: IconButton(
+                          iconSize: 25.0,
+                          icon: Icon(Icons.done, color: Colors.white),
+                          onPressed: this.editing == true ? null : _readyButtonPressed,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _readyButtonPressed() {
+    setState(() {
+      print(todos);
+      this.readyButtonPressed = true;
+    });
+  }
+
+  void _handleSubmitted(String text) {
+    setState(() {
+      this.editing = text.length == 0 ? true : false;
+      todo.title = text;
+      todo.editing = this.editing;
+    });
+  }
+}
---
+++ flutter_todo_app/lib/todo_list_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'todo.dart';
+
+class ToDoListScreen extends StatefulWidget {
+
+  ToDoListScreen({Key key}) : super(key: key);
+
+  @override
+  State createState() => ToDoListScreenState();
+}
+
+List<Todo> todos = <Todo>[];
+int count = 0;
+
+class ToDoListScreenState extends State<ToDoListScreen>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(appTitle),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.only(bottom: 75.0),
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: ListView.builder(
+              itemCount: todos.length,
+              itemBuilder: (context, index) => todos[index],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: Container(
+        height: MediaQuery.of(context).viewInsets.bottom == 0 ? 70.0 : 0.0,
+        width: MediaQuery.of(context).viewInsets.bottom == 0 ? 70.0 : 0.0,
+        child: FloatingActionButton(
+          tooltip: 'Add',
+          child: Icon(Icons.add, size: 35.0,),
+          onPressed: () {
+            setState(() {
+              Todo todo = Todo(index: count);
+              todos.add(todo);
+              count += 1;
+            });
+          },
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
