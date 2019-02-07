74a3afffbfaedf6d16c542beb0b7736623647647
Alordead <spealcy@gmail.com>
2019-02-07 13:06:57 +0300

    button-animations: complete animated floating action button

--- flutter_animated_button/lib/animated_floating_action_button.dart
+++ flutter_animated_button/lib/animated_floating_action_button.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AnimatedFloatingActionButton extends StatefulWidget {
+  final Function() onPressed;
+  final String tooltip;
+  final IconData icon;
+
+  AnimatedFloatingActionButton({this.onPressed, this.tooltip, this.icon});
+
   @override
   _AnimatedFloatingActionButtonState createState() => _AnimatedFloatingActionButtonState();
 }
@@ -12,7 +18,9 @@ class _AnimatedFloatingActionButtonState extends State<AnimatedFloatingActionBut
   AnimationController _animationController;
   Animation<Color> _animateColor;
   Animation<double> _animateIcon;
+  Animation<double> _translateButton;
   Curve _curve = Curves.easeOut;
+  double _fabHeight = 56.0;
 
   @override
   void initState() {
@@ -24,7 +32,16 @@ class _AnimatedFloatingActionButtonState extends State<AnimatedFloatingActionBut
     _animateColor = ColorTween(
       begin: Colors.lightBlue,
       end: Colors.redAccent,
-    ).animate(CurvedAnimation(parent: _animationController, curve: Interval(0.00, 1.00, curve: _curve),
+    ).animate(CurvedAnimation(parent: _animationController, curve: Interval(0.00, 1.00, curve: Curves.linear),
+    ));
+    _translateButton = Tween<double>(
+      begin: _fabHeight,
+      end: -14.0,
+    ).animate(CurvedAnimation(parent: _animationController, curve: Interval(
+        0.0,
+        0.75,
+        curve: _curve
+      ),
     ));
     super.initState();
   }
@@ -44,18 +61,65 @@ class _AnimatedFloatingActionButtonState extends State<AnimatedFloatingActionBut
     isOpened = !isOpened;
   }
 
-    Widget toggle() {
-      return FloatingActionButton(
-        backgroundColor: _animateColor.value,
-        onPressed: animate,
-        tooltip: 'Toggle',
-        child: AnimatedIcon(icon: AnimatedIcons.menu_close, progress: _animateIcon
-        ),
-      );
-    }
+  Widget add() {
+    return Container(
+      child: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget image() {
+    return Container(
+      child: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Image',
+        child: Icon(Icons.image),
+      ),
+    );
+  }
+
+  Widget inbox() {
+    return Container(
+      child: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Inbox',
+        child: Icon(Icons.inbox),
+      ),
+    );
+  }
+
+  Widget toggle() {
+    return FloatingActionButton(
+      backgroundColor: _animateColor.value,
+      onPressed: animate,
+      tooltip: 'Toggle',
+      child: AnimatedIcon(icon: AnimatedIcons.menu_close, progress: _animateIcon
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return toggle();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Transform(
+          transform: Matrix4.translationValues(0.0, _translateButton.value * 3.0, 0.0),
+          child: add(),
+        ),
+        Transform(
+          transform: Matrix4.translationValues(0.0, _translateButton.value * 2.0, 0.0),
+          child: image(),
+        ),
+        Transform(
+          transform: Matrix4.translationValues(0.0, _translateButton.value, 0.0),
+          child: inbox(),
+        ),
+        toggle(),
+      ],
+    );
   }
 }
