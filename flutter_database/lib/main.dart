aeea121744f27b65ec2819b0823e99dd9157ed66
Alordead <spealcy@gmail.com>
2019-02-08 14:28:05 +0300

    init flutter database app

---
+++ flutter_database/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+final ThemeData kDefaultThemeData = ThemeData(
+  primaryColor: Colors.redAccent,
+  primarySwatch: Colors.red,
+  primaryColorBrightness: Brightness.dark,
+);
+
+void main() => runApp(DBSampleApp());
+
+class DBSampleApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Database Sample',
+      theme: kDefaultThemeData,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text("Hello flutter!"),
+      ),
+    );
+  }
+}
